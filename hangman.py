e0b47a91463538bd881e0cde456e2a8b16e932fd
Lana <lana.semecnik@gmail.com>
2021-11-29 23:08:58 +0100

    reworking code in hangman and index

--- hangman.py
+++ hangman.py
@@ -20,14 +20,15 @@ def get_wordlist():
 
 # check if length of word is even in the list of words
 def difficulty_check(wordlist, user_difficulty):
-    for counter, word in enumerate(wordlist, start=1):
-        if len(word) == user_difficulty:
-            break
-        counter += 1
-        if counter == len(wordlist):
-            print_inputcheck_invalid = "Sorry, our list doesn't have any words of that length!\n" \
-                                 "Please restart the program and try again."
-            return print_inputcheck_invalid
+    if user_difficulty < 4:
+        return "Please choose a length of 4 letters or more"
+    else:
+        for counter, word in enumerate(wordlist, start=1):
+            if len(word) == user_difficulty:
+                return False
+            counter += 1
+            if counter == len(wordlist):
+                return "Sorry, our list doesn't have any words of that length"
 
 
 # choose word of that length from list
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+
+import hangman
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+    elif request.method == 'POST':
+        difficulty = int(request.form.get('difficulty'))
+        wordlist = hangman.get_wordlist()
+        error = hangman.difficulty_check(wordlist, difficulty)
+        if error is False:
+            response = make_response(render_template("/game"))
+            response.set_cookie("user_difficulty", str(difficulty))
+            return response
+        else:
+            return render_template("index.html", error=error)
+
+
+@app.route("/game", methods=['POST', 'GET'])
+def game():
+    if request.method == 'GET':
+        return render_template("game.html")
+    if request.method == 'POST':
+        return render_template("game.html")
 
 
 if __name__ == '__main__':
