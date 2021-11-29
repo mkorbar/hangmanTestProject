073ec95a576b8b21b7459943b35eb6b559d4b0a7
Lana <lana.semecnik@gmail.com>
2021-11-29 23:10:02 +0100

    debugging

--- main.py
+++ main.py
@@ -14,19 +14,9 @@ def index():
         wordlist = hangman.get_wordlist()
         error = hangman.difficulty_check(wordlist, difficulty)
         if error is False:
-            response = make_response(render_template("/game"))
-            response.set_cookie("user_difficulty", str(difficulty))
-            return response
+            return render_template("index.html", error=error)
         else:
             return render_template("index.html", error=error)
-
-
-@app.route("/game", methods=['POST', 'GET'])
-def game():
-    if request.method == 'GET':
-        return render_template("game.html")
-    if request.method == 'POST':
-        return render_template("game.html")
 
 
 if __name__ == '__main__':
